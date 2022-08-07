aec28500463a22c64b0ceb184f624dbdaecd477e
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2022-08-07 19:30:24 -0400

    Feito a troca do botão por um input e feito a implementação da mudança de posição

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const handleChangeL = () => {
+const handleChangeLeft = () => {
   const coordenadas = document.getElementById("location").textContent;
   const lat = coordenadas.split(",");
 
@@ -18,7 +18,7 @@ const handleChangeL = () => {
   }
   document.getElementById("location").innerText = lat.join(",");
 };
-const handleChangeR = () => {
+const handleChangeRight = () => {
   const coordenadas = document.getElementById("location").textContent;
   const lat = coordenadas.split(",");
 
@@ -38,3 +38,46 @@ const handleChangeR = () => {
   }
   document.getElementById("location").innerText = lat.join(",");
 };
+const handleChangePosition = () => {
+  const coordenadas = document.getElementById("location").textContent;
+  const lat = coordenadas.split(",");
+
+  switch (lat[2]) {
+    case "N":
+      if (lat[1] < 5) {
+        lat[1]++;
+      }
+      break;
+    case "S":
+      if (lat[1] > 0) {
+        lat[1]--;
+      }
+      break;
+    case "E":
+      if (lat[0] < 5) {
+        lat[0]++;
+      }
+      break;
+    case "W":
+      if (lat[0] > 0) {
+        lat[0]--;
+      }
+      break;
+  }
+
+  document.getElementById("location").innerText = lat.join(",");
+};
+function control() {
+  const coordenadas = document.getElementById("control").value;
+
+  const lat = coordenadas.split("");
+  for (let i = 0; i < lat.length; i++) {
+    if (lat[i] === "L") {
+      handleChangeLeft();
+    } else if (lat[i] === "R") {
+      handleChangeRight();
+    } else if (lat[i] === "M") {
+      handleChangePosition();
+    }
+  }
+}
